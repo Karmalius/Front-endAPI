72461d30a0fdb6e4c2e8a6b3edbfe6599859c73b
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-04-20 16:45:15 +0300

    Added button and onclick

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,79 @@
 import React, { useState, useEffect } from 'react';
 import "./App.css"
 import { Table } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+
+
 
 const App = () => {
-  const [muuttuja, setMuuttuja] = useState([])
 
-  useEffect(() => {
-    fetch("https://ficlibrary.herokuapp.com/api/getall")
-      .then((result) => {
-        return result.json();
-      })
-      .then((data) => {
-        setMuuttuja(data)
-      });
-  }, [])
+  const [muuttuja, setMuuttuja] = useState([])
+  const [toinenMuuttuja, setToinenMuuttuja] = useState()
+  
+
+  const GetData = () => {
+  
+    useEffect(() => {
+      fetch("https://ficlibrary.herokuapp.com/api/getall")
+        .then((result) => {
+          return result.json();
+        })
+        .then((data) => {
+          setMuuttuja(data)
+        });
+    }, [])
+  
+    return (
+      <>
+          <div>
+            <Table striped bordered>
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Pairing</th>
+                  <th>Plot</th>
+                  <th>Chapters</th>
+                  <th>Link</th>
+                </tr>
+              </thead>
+              <tbody>
+                {muuttuja.map((data) => (
+                  <tr>
+                    <td key={data.name}> {data.name} </td>
+                    <td key={data.pairing}> {data.pairing} </td>
+                    <td key={data.plot}> {data.plot} </td>
+                    <td key={data.chapters}> {data.chapters} </td>
+                    <td key={data.link}> {data.link} </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </div>
+      </>
+    );
+  }
+
+  const handleClick = () => {
+    GetData();
+    setToinenMuuttuja(muuttuja);
+  };
 
   return (
     <>
-      {
         <div>
-      <Table striped bordered responsive>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Pairing</th>
-            <th>Plot</th>
-            <th>Chapters</th>
-            <th>Link</th>
-          </tr>
-        </thead>
-        <tbody>
-          {muuttuja.map((data) => (
-            <tr>
-              <td> {data.name} </td>
-              <td> {data.pairing} </td>
-              <td> {data.plot} </td>
-              <td> {data.chapters} </td>
-              <td> {data.link} </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-      </div>
-}
+          <form>
+            <div>
+              <Button
+                type="button"
+                onClick={handleClick}
+                variant="dark"
+              >
+                Show all fics
+              </Button>
+            </div>
+          </form>
+        </div>
+        <div>{toinenMuuttuja}</div>
     </>
   );
 }
