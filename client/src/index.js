6519b8f522ae00eb119b10898e233f6b8490f55a
Karmalius <55229180+Misumuru@users.noreply.github.com>
2021-04-26 09:26:06 +0300

    Added comments

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
+// Otetaan käyttöön react-kirjastoja, haetaan CSS-tyylitiedosto ja haetaan App-tiedosto
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
+// Pyydetään reactia renderöimään App-komponentti HTML-sivulta löytyvään 'root'-elementtiin.
 ReactDOM.render(
     <App />,
   document.getElementById('root')
